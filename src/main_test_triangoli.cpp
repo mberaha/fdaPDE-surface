ea282ac9726769e080c81beb90a567cb361a6a7a
mariob6 <berahamario@gmail.com>
2016-12-29 18:54:39 +0100

    testing desktop computer github access

--- src/main_test_triangoli.cpp
+++ src/main_test_triangoli.cpp
@@ -4,32 +4,32 @@
 #include <vector>
 
 int main()
-{
+{ //! Ciao Anna :P
 	/*Point a(1,1,0,0,0);
 	Point b(2,2,0,1,0);
 	Point c(3,3,0,0,1);
-	
+
 	a.print(std::cout);
 	b.print(std::cout);
 	c.print(std::cout);
-	
+
 	std::cout<<b.getId()<<std::endl;
-	b.print(std::cout);	
-	
+	b.print(std::cout);
+
 	std::vector<Point> points;
 	points.push_back(c);
 	points.push_back(b);
 	points.push_back(a);
 
 	Triangle<3,2,3> t(1,points);
-	
+
 	std::cout<<"I punti sono"<<std::endl;
 	for (int i=0; i<3; ++i)
 		points[i].print(std::cout);
 	std::cout<<std::endl;
-	
+
 	//t.print(std::cout);
-	
+
 	Point P(4,4,0,0.25,0.25);
 	P.print(std::cout);
 
@@ -37,21 +37,21 @@ int main()
 
 	for (int i=0; i<3; ++i)
 		std::cout<<BaryHope[i]<<std::endl;
-		
+
 	bool verita = t.isPointInside(P);
 	std::cout<< "il punto è dentro iff "<<verita<<std::endl;	*/
-	
+
 	/////
 	//! Test sulla mesh
-	
+
 	std::string filename("Caramella.csv");
 	std::cout<<"filename = "<<filename<<"\n";
 	std::cout<<"calling the mesh"<<"\n";
 	MeshHandler<1,2,3> mesh(filename);
 	std::cout<<"finished importing the mesh"<<"\n";
-	
+
 	mesh.printPoints(std::cout);
 	mesh.printTriangles(std::cout);
-	
+
 return 0;
 }
