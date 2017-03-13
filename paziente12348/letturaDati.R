f791e7b19de1e650cc8090182e48c59240564e99
AnnaCosmo <pacs_student@pacs-virtual-machine.(none)>
2017-03-13 20:27:34 +0100

    added stress files and fixed R code to create them

--- paziente12348/letturaDati.R
+++ paziente12348/letturaDati.R
@@ -11,38 +11,42 @@ write_mesh_file <- function(filename=NULL,nodes,triangles){
   cat("\n",file=filename, append = T)
   write.table(triangles,file=filename,sep=",",row.names = F, col.names = F,append = TRUE)
 }
+
+write_stress_file <- function(filename=NULL,stresses){
+  if(is.null(filename))
+    filename='mystress.csv'
+  write.table(stresses,file=filename,sep=",",row.names = F, col.names = F,append = TRUE)
+}
+
 #setwd("C:\Users\Utente\Desktop\AnuRisk\12348\wss_vtk")
 for(i in 211:220){
 file =paste0("wss_",toString(i),".mat")
 filemat<-readMat(file)
-filename = paste0("wss_",toString(i),".csv")
+filename = paste0("geom_",toString(i),".csv")
+stressfile = paste0("wss_",toString(i),".csv")
 write_mesh_file(filename=filename,nodes=filemat$V,triangles=filemat$T)
+write_stress_file(filename=stressfile,stresses=filemat$wss)
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-read.mesh<-function(filename){
-  nnodes = read.table(filename, nrows = 2, header = FALSE, sep =' ', stringsAsFactors = FALSE)[1,2]
-  ntriangles = read.table(filename, nrows = 2, header = FALSE, sep =' ', stringsAsFactors = FALSE)[2,2]
-  geometry = read.csv(filename, skip=2, header= FALSE)
-  nodes = geometry[1:nnodes,]
-  triangles = geometry[(nnodes+1):(nnodes+ntriangles),]
+#read.mesh<-function(filename){
+ # nnodes = read.table(filename, nrows = 2, header = FALSE, sep =' ', stringsAsFactors = FALSE)[1,2]
+ # ntriangles = read.table(filename, nrows = 2, header = FALSE, sep =' ', stringsAsFactors = FALSE)[2,2]
+ # geometry = read.csv(filename, skip=2, header= FALSE)
+ # nodes = geometry[1:nnodes,]
+ # triangles = geometry[(nnodes+1):(nnodes+ntriangles),]
   
-  retlist = list(nnodes=nnodes,ntriangles=ntriangles,nodes=nodes,triangles=triangles)
-  return(retlist)
-}
+ # retlist = list(nnodes=nnodes,ntriangles=ntriangles,nodes=nodes,triangles=triangles)
+ # return(retlist)
+#}
 
-mesh=read.mesh(filename)
+#mesh=read.mesh(filename)
+
+
+
+#mesh=read.mesh(filename)
+
 
 
 #### CREATING A MESH FILE FROM NODES AND CONNECTIVITY #####
